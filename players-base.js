9e01c37e2c25f18f32ba60eb00da13e14d4f4ac6
Gionata1998 <iphonegionata@gmail.com>
2026-08-21 22:06:10 +0200

    Add FantAsta v10 base player dataset

---
+++ players-base.js
@@ -0,0 +1,300 @@
+window.FANTA_BASE=`P|Svilar|Roma|18
+P|Martinez Jo.|Inter|17
+P|Carnesecchi|Atalanta|16
+P|Butez|Como|16
+P|Vicario|Juventus|16
+P|Maignan|Milan|15
+P|De Gea|Fiorentina|13
+P|Meret|Napoli|11
+P|Skorupski|Bologna|10
+P|Caprile|Cagliari|9
+P|Di Gregorio|Juventus|9
+P|Mandas|Lazio|9
+P|Okoye|Udinese|9
+P|Bijlow|Genoa|8
+P|Falcone|Lecce|8
+P|Daffara|Parma|7
+P|Muric|Sassuolo|7
+P|Perin|Juventus|6
+P|Stankovic F.|Venezia|6
+P|Thiam|Monza|5
+P|Milinkovic-Savic V.|Napoli|5
+P|Palmisani|Frosinone|4
+D|Dimarco|Inter|32
+D|Molina N.|Roma|18
+D|Wesley|Roma|17
+D|Akanji|Inter|16
+D|Bremer|Juventus|15
+D|Mancini|Roma|15
+D|Bastoni|Inter|14
+D|Pavlovic|Milan|14
+D|Rrahmani|Napoli|14
+D|Kalulu|Juventus|13
+D|N'Dicka|Roma|13
+D|Solet|Udinese|13
+D|Stones|Inter|12
+D|Gila|Milan|12
+D|Di Lorenzo|Napoli|12
+D|Spence|Inter|12
+D|Ostigard|Genoa|11
+D|Bisseck|Inter|11
+D|Scalvini|Atalanta|10
+D|Ramon|Como|10
+D|Dodò|Fiorentina|10
+D|Hermoso|Roma|10
+D|Vasquez|Genoa|9
+D|Cambiaso|Juventus|9
+D|Chalobah T.|Como|9
+D|Hien|Atalanta|8
+D|Zappacosta|Atalanta|8
+D|Lucumì|Juventus|8
+D|Miranda J.|Bologna|8
+D|Mina|Cagliari|8
+D|Dragusin|Fiorentina|8
+D|Jimenez A.|Fiorentina|8
+D|Norton-Cuffy|Genoa|8
+D|Celik|Juventus|8
+D|Bartesaghi|Milan|8
+D|Spinazzola|Napoli|8
+D|Delprato|Parma|8
+D|Valeri|Parma|8
+D|Koulierakis|Roma|8
+D|Vojvoda|Udinese|8
+D|Couto|Como|8
+D|Obert|Cagliari|7
+D|Kaiki|Como|7
+D|Carlos Augusto|Inter|7
+D|Doekhi|Lazio|7
+D|Romagnoli|Lazio|7
+D|Tiago Gabriel|Lecce|7
+D|Gabbia|Milan|7
+D|Tomori|Milan|7
+D|Buongiorno|Napoli|7
+D|Idzes|Sassuolo|7
+D|Coco|Torino|7
+D|Ismajli|Torino|7
+D|Kristensen T.|Atalanta|7
+D|Ahanor|Atalanta|6
+D|Bellanova|Atalanta|6
+D|Bernasconi|Atalanta|6
+D|Heggem|Bologna|6
+D|Zortea|Bologna|6
+D|Valle|Como|6
+D|Valdepenas|Fiorentina|6
+D|Viery|Fiorentina|6
+D|Monterisi|Frosinone|6
+D|Marcandalli|Genoa|6
+D|Pavard|Inter|6
+D|Tavares N.|Lazio|6
+D|Marusic|Lazio|6
+D|Gallo|Lecce|6
+D|Mangas|Monza|6
+D|Beukema|Napoli|6
+D|Comuzzo|Torino|6
+D|Kamara H.|Udinese|6
+D|Bella-Kotchap|Venezia|6
+D|Obrador|Sassuolo|6
+D|Kolasinac|Atalanta|5
+D|Vitik|Bologna|5
+D|Holm|Bologna|5
+D|Kofler|Cagliari|5
+D|Zè Pedro|Cagliari|5
+D|Kempf|Como|5
+D|Parisi|Fiorentina|5
+D|Bracaglia|Frosinone|5
+D|Oyono A.|Frosinone|5
+D|Martin|Genoa|5
+D|Kelly L.|Juventus|5
+D|Pedraza|Lazio|5
+D|Gaspar K.|Lecce|5
+D|Veiga D.|Lecce|5
+D|Olivera|Napoli|5
+D|Rensch|Roma|5
+D|Pedersen|Torino|5
+D|Moreno M.|Venezia|5
+D|Haps|Venezia|5
+D|Fortini|Torino|5
+C|Paz N.|Como|30
+C|McTominay|Napoli|28
+C|Calhanoglu|Inter|27
+C|Orsolini|Bologna|26
+C|Pulisic|Milan|25
+C|Rabiot|Milan|22
+C|Baturina|Como|19
+C|Mora|Roma|19
+C|Da Cunha|Como|18
+C|Zaniolo|Udinese|18
+C|Atta|Fiorentina|17
+C|Barella|Inter|17
+C|McKennie|Juventus|17
+C|Zaccagni|Lazio|16
+C|De Bruyne|Napoli|15
+C|Vlasic|Torino|14
+C|Gudmundsson A.|Fiorentina|13
+C|Taylor K.|Lazio|13
+C|Ederson D.S.|Atalanta|12
+C|Samardzic|Atalanta|12
+C|Rodriguez Je.|Como|12
+C|Alajbegovic|Juventus|12
+C|Conceicao|Juventus|12
+C|Modric|Milan|12
+C|Mastantuono|Fiorentina|12
+C|Moreira|Milan|12
+C|Rowe|Bologna|11
+C|Baldanzi|Genoa|11
+C|Zambo Anguissa|Napoli|11
+C|Perrone|Como|10
+C|Zielinski|Inter|10
+C|Thuram K.|Juventus|10
+C|Saelemaekers|Milan|10
+C|Politano|Napoli|10
+C|Konè M.|Roma|10
+C|Thorstvedt|Sassuolo|10
+C|Casadei|Torino|10
+C|Ekkelenkamp|Udinese|10
+C|Pellegrini Lo.|Roma|10
+C|Pasalic|Atalanta|9
+C|Bernardeschi|Bologna|9
+C|Mandragora|Fiorentina|9
+C|Cancellieri|Lazio|9
+C|Isaksen|Lazio|9
+C|Konè I.|Sassuolo|9
+C|Odgaard|Bologna|8
+C|Cambiaghi|Bologna|8
+C|Fagioli|Fiorentina|8
+C|Ndour|Fiorentina|8
+C|Diouf|Inter|8
+C|Sucic P.|Inter|8
+C|Locatelli|Juventus|8
+C|Colpani|Monza|8
+C|Vergara|Napoli|8
+C|Cristante|Roma|8
+C|Schmid|Frosinone|8
+C|Gaetano|Atalanta|7
+C|Ferguson|Bologna|7
+C|Fazzini|Cagliari|7
+C|Calò|Frosinone|7
+C|Frendrup|Genoa|7
+C|Frattesi|Lazio|7
+C|Pessina|Monza|7
+C|Lobotka|Napoli|7
+C|Bernabè|Parma|7
+C|Volpato|Sassuolo|7
+C|Oristanio|Torino|7
+C|Unai Gomez|Udinese|7
+C|Sow|Genoa|7
+C|Zalewski|Atalanta|6
+C|Pobega|Bologna|6
+C|Adopo|Cagliari|6
+C|Winks|Cagliari|6
+C|Caqueret|Como|6
+C|Liberali|Como|6
+C|Oulai|Fiorentina|6
+C|Ellertsson|Genoa|6
+C|Zhegrova|Juventus|6
+C|Rovella|Lazio|6
+C|Coulibaly L.|Lecce|6
+C|Pierotti|Lecce|6
+C|Chukwueze|Milan|6
+C|Akinsanmiro|Monza|6
+C|Nicolussi Caviglia|Parma|6
+C|Karlstrom|Udinese|6
+C|Basic|Venezia|6
+C|Tourè I.|Monza|6
+C|Amondarain|Bologna|5
+C|Romano|Cagliari|5
+C|Felici|Cagliari|5
+C|Milla|Como|5
+C|Addai|Como|5
+C|Zerbin|Frosinone|5
+C|Meichtry|Genoa|5
+C|Traorè Hj.|Genoa|5
+C|Mkhitaryan|Inter|5
+C|Koopmeiners|Juventus|5
+C|Dele-Bashiru|Lazio|5
+C|Berisha M.|Lecce|5
+C|Gandelman|Lecce|5
+C|Fofana Y.|Milan|5
+C|Keita M.|Parma|5
+C|Pisilli|Roma|5
+C|Matic|Sassuolo|5
+C|Adzic|Sassuolo|5
+C|Bakola|Sassuolo|5
+C|Cacciamani|Torino|5
+C|Gineitis|Torino|5
+C|Piotrowski|Udinese|5
+C|Busio|Venezia|5
+C|Sohm|Venezia|5
+C|Perez K.|Venezia|5
+C|Grillitsch|Frosinone|5
+A|Martinez L.|Inter|35
+A|Malen|Roma|34
+A|Thuram|Inter|29
+A|Hojlund|Napoli|28
+A|Ramos G.|Milan|27
+A|Kolo Muani|Juventus|26
+A|Kean|Fiorentina|25
+A|Yildiz|Juventus|23
+A|Douvikas|Como|20
+A|Scamacca|Atalanta|19
+A|Davis K.|Udinese|19
+A|Krstovic|Atalanta|18
+A|Leao|Milan|18
+A|Berardi|Sassuolo|18
+A|De Ketelaere|Atalanta|17
+A|Dovbyk|Bologna|16
+A|Esposito F.P.|Inter|16
+A|Pellegrino M.|Fiorentina|15
+A|Laurientè|Sassuolo|15
+A|Simeone|Torino|15
+A|Santos A.|Napoli|14
+A|Dybala|Roma|14
+A|Castro S.|Roma|14
+A|Raspadori|Atalanta|13
+A|Esposito Se.|Cagliari|13
+A|Nkunku|Milan|13
+A|Pinamonti|Sassuolo|13
+A|Kevin Carlos|Cagliari|13
+A|Soulè|Roma|12
+A|Adams A.|Venezia|12
+A|Diao|Como|11
+A|Colombo|Genoa|11
+A|Tourè E.|Parma|11
+A|Dia|Lazio|10
+A|Ratkov|Lazio|10
+A|Bowie|Sassuolo|10
+A|Romero D.|Parma|10
+A|Ghedjemis|Frosinone|9
+A|David|Juventus|9
+A|Geubbels|Lecce|9
+A|Cutrone|Monza|9
+A|Adams C.|Torino|9
+A|Piccoli|Bologna|8
+A|Vitinha O.|Genoa|8
+A|Yeboah J.|Venezia|8
+A|Rrahmani Al.|Venezia|8
+A|Raimondo|Frosinone|7
+A|Bonny|Inter|7
+A|Stulic|Lecce|7
+A|Zapata D.|Torino|7
+A|Sulemana K.|Atalanta|6
+A|Mutandwa|Cagliari|6
+A|Boga|Juventus|6
+A|Noslin|Lazio|6
+A|Gimenez|Milan|6
+A|Neres|Napoli|6
+A|Maldini|Cagliari|5
+A|Camarda|Milan|5
+A|Mota|Monza|5
+A|Frigan|Parma|5
+A|Adorante|Venezia|5
+A|Milik|Juventus|5
+A|Borrelli|Cagliari|4
+A|Mendy P.|Cagliari|4
+A|Morata|Como|4
+A|Kvernadze|Frosinone|4
+A|Havel|Genoa|4
+A|Giovane|Napoli|4
+A|Kulenovic|Torino|4
+A|Gueye|Udinese|4
+A|Robinson J.|Monza|4`;
