d6dbc75c61a341d23a5f565ae23d1e4f60cfc089
Gionata1998 <iphonegionata@gmail.com>
2026-08-21 22:04:01 +0200

    Add complete low-quotation player dataset for v10

---
+++ players-extra.js
@@ -0,0 +1,205 @@
+window.FANTA_EXTRA=`D|Arizala|Udinese|4
+D|Bertola|Udinese|4
+D|Birindelli|Monza|4
+D|Calvani|Frosinone|4
+D|Comert|Torino|4
+D|Correia T.|Venezia|4
+D|Delli Carri|Monza|4
+D|Doig|Sassuolo|4
+D|Gatti|Juventus|4
+D|Ghilardi|Roma|4
+D|Halhal|Venezia|4
+D|Kabasele|Udinese|4
+D|Lucchesi|Monza|4
+D|Mitaj|Genoa|4
+D|Pongracic|Fiorentina|4
+D|Siebert|Lecce|4
+D|Smolcic I.|Como|4
+D|Valenti|Parma|4
+D|Walukiewicz|Sassuolo|4
+D|Zanoli|Udinese|4
+D|Zappa|Cagliari|4
+C|Almqvist|Parma|4
+C|Amorim|Genoa|4
+C|Cataldi|Lazio|4
+C|Cichella|Frosinone|4
+C|De Roon|Atalanta|4
+C|Douglas Luiz|Juventus|4
+C|El Aynaoui|Roma|4
+C|Fabbian|Fiorentina|4
+C|Fitz-Jim|Torino|4
+C|Folorunsho|Napoli|4
+C|Ilkhan|Torino|4
+C|Jashari|Milan|4
+C|Koutsoupias|Frosinone|4
+C|Loftus-Cheek|Milan|4
+C|Luis Henrique|Inter|4
+C|Moro N.|Bologna|4
+C|Ngom|Lecce|4
+C|Njie|Torino|4
+C|Prati|Cagliari|4
+C|Ricci S.|Milan|4
+D|Akpoguma|Frosinone|3
+D|Athekame|Milan|3
+D|Britschgi|Parma|3
+D|Candè|Sassuolo|3
+D|Carboni A.|Monza|3
+D|Casale|Bologna|3
+D|Cittadini|Frosinone|3
+D|De Winter|Milan|3
+D|Estupinan|Milan|3
+D|Floriani Mussolini|Lazio|3
+D|Hainaut|Venezia|3
+D|Idrissi R.|Cagliari|3
+D|Joao Mario|Fiorentina|3
+D|Kossounou|Atalanta|3
+D|Kouadio|Monza|3
+D|Provstgaard|Lazio|3
+D|Ranieri L.|Fiorentina|3
+D|Rodriguez Ju.|Cagliari|3
+D|Schingtienne|Venezia|3
+D|Sverko|Venezia|3
+D|Troilo|Parma|3
+C|Aboukhlal|Torino|3
+C|Brescianini|Fiorentina|3
+C|Colombo L.|Monza|3
+C|Deiola|Cagliari|3
+C|Diallo O.|Parma|3
+C|Dominguez B.|Bologna|3
+C|Fini|Frosinone|3
+C|Gelli F.|Frosinone|3
+C|Gilmour|Napoli|3
+C|Helgason|Venezia|3
+C|Messias|Genoa|3
+C|Miller L.|Udinese|3
+C|Miretti|Juventus|3
+C|Sorensen O.|Parma|3
+C|Stankovic A.|Inter|3
+C|Sulemana I.|Atalanta|3
+A|Ekhator|Juventus|3
+A|Elphege|Parma|3
+A|Kuhn|Como|3
+A|Lang|Napoli|3
+A|Lucca|Napoli|3
+A|N'Dri|Lecce|3
+A|Varela G.|Monza|3
+D|Alhassane|Bologna|2
+D|Biraghi|Torino|2
+D|Diawara S.|Milan|2
+D|Ebosse|Udinese|2
+D|Franjic|Venezia|2
+D|Helland|Bologna|2
+D|Jean|Lecce|2
+D|Lazzari|Lazio|2
+D|Marin R.|Napoli|2
+D|Otoa|Genoa|2
+D|Palma|Udinese|2
+D|Pellegrini Lu.|Lazio|2
+D|Puczka|Genoa|2
+D|Sabelli|Genoa|2
+D|Sagrado|Venezia|2
+D|Van Der Brempt|Como|2
+C|Boloca|Sassuolo|2
+C|Chakvetadze|Udinese|2
+C|Ciurria|Monza|2
+C|Duncan|Venezia|2
+C|El Azzouzi O.|Bologna|2
+C|Fadera|Como|2
+C|Hasa|Frosinone|2
+C|Lipani|Sassuolo|2
+C|Maleh|Lecce|2
+C|Masini|Genoa|2
+C|Musah|Milan|2
+C|Ordonez C.|Parma|2
+C|Venturino|Genoa|2
+P|Desplanches|Frosinone|2
+P|Provedel|Inter|2
+A|Buksa|Udinese|2
+A|Petagna|Monza|2
+D|Abankwah|Udinese|1
+D|Amey|Frosinone|1
+D|Antov|Monza|1
+D|Bakoune|Monza|1
+D|Cabal|Juventus|1
+D|Carboni F.|Parma|1
+D|Corrado|Frosinone|1
+D|Cuenca A.|Como|1
+D|De Silvestri|Bologna|1
+D|Gelli J.|Frosinone|1
+D|Goldaniga|Como|1
+D|Gomes|Venezia|1
+D|Marianucci|Napoli|1
+D|Matturro|Genoa|1
+D|Mazzocchi|Napoli|1
+D|Missori|Sassuolo|1
+D|Mlacic|Udinese|1
+D|Ndaba|Lecce|1
+D|Ndiaye|Parma|1
+D|Oyono J.|Frosinone|1
+D|Patric|Lazio|1
+D|Perez M.|Lecce|1
+D|Pieragnolo|Sassuolo|1
+D|Raterink|Cagliari|1
+D|Rugani|Juventus|1
+D|Vogliacco|Genoa|1
+D|Ziolkowski|Roma|1
+C|Anjorin|Torino|1
+C|Belahyane|Lazio|1
+C|Bjarkason|Venezia|1
+C|Camara A.|Udinese|1
+C|Cremaschi|Parma|1
+C|Dagasso|Venezia|1
+C|El Azzouzi A.|Frosinone|1
+C|Fofana Sa.|Lecce|1
+C|Gorter|Lecce|1
+C|Iannoni|Sassuolo|1
+C|Ilic|Torino|1
+C|Kaba|Lecce|1
+C|Kone B.|Frosinone|1
+C|Lahdo|Como|1
+C|Liteta|Cagliari|1
+C|Przyborek|Lazio|1
+C|Zarraga|Udinese|1
+P|Christensen O.|Fiorentina|1
+P|Ciocci|Cagliari|1
+P|Contini|Napoli|1
+P|Corvi|Parma|1
+P|De Marzi|Roma|1
+P|Di Gennaro|Inter|1
+P|Gollini|Roma|1
+P|Grandi|Venezia|1
+P|Happonen|Bologna|1
+P|Lezzerini|Fiorentina|1
+P|Lolic|Frosinone|1
+P|Mascardi|Torino|1
+P|Motta|Lazio|1
+P|Padelli|Udinese|1
+P|Paleari|Torino|1
+P|Pessina Mas.|Bologna|1
+P|Piana|Udinese|1
+P|Pinsoglio|Juventus|1
+P|Pizzignacco|Monza|1
+P|Pozzi|Venezia|1
+P|Renzetti|Lazio|1
+P|Rossi F.|Atalanta|1
+P|Russo A.|Sassuolo|1
+P|Samooja|Lecce|1
+P|Sherri|Cagliari|1
+P|Siviero|Torino|1
+P|Sommariva|Genoa|1
+P|Sportiello|Atalanta|1
+P|Stolz|Genoa|1
+P|Strajnar|Monza|1
+P|Terracciano|Milan|1
+P|Tornqvist|Como|1
+P|Torriani|Milan|1
+P|Turati|Sassuolo|1
+P|Vigorito|Como|1
+A|Albarracin|Cagliari|1
+A|Azon|Como|1
+A|Bayo V.|Udinese|1
+A|Lauberbach|Venezia|1
+A|Lisman|Venezia|1
+A|Moro L.|Sassuolo|1
+A|Trepy|Cagliari|1
+A|Vaz|Roma|1`;
